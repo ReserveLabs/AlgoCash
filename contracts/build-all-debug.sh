6bddf4732f5826fbd52183601ebbfa3f879bed44
lab@quantiex.com <lab@quantiex.com>
2021-07-09 12:36:04 +0800

    add tokenstub,distribute contracts.

--- contracts/build-all-debug.sh
+++ contracts/build-all-debug.sh
@@ -5,5 +5,6 @@ set -eu
 cargo +nightly contract build --debug --manifest-path util/Cargo.toml
 cargo +nightly contract build --debug --manifest-path asset/Cargo.toml
 cargo +nightly contract build --debug --manifest-path oracle/Cargo.toml
+cargo +nightly contract build --debug --manifest-path distributor/Cargo.toml
 cargo +nightly contract build --debug --manifest-path boardroom/Cargo.toml
 cargo +nightly contract build --debug --manifest-path treasury/Cargo.toml
